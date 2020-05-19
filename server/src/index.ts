54bee7ab796d1bfeedcdb108aaf4579e610796dc
truong nguyen <n.h.t.94.e@gmail.com>
2020-05-19 17:12:51 +0700

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+
+import express, { Application } from "express";
+import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database";
+import { typeDefs, resolvers } from "./graphql";
+
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
+  server.applyMiddleware({ app, path: "/api" });
+
+  app.listen(process.env.PORT);
+
+  console.log(`[app]: https://localhost:${process.env.PORT}`);
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
+};
+
+mount(express());
