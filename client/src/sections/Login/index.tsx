7815e3cecbc3dce54a789fff9583b768e358dddd
NHT <n.h.t.94.e@gmail.com>
2020-09-30 17:27:01 +0700

    update login

--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import { Card, Layout, Typography } from "antd";
+import React, { useEffect, useRef } from "react";
+import { Card, Layout, Spin, Typography } from "antd";
 import { useApolloClient, useMutation } from "@apollo/react-hooks";
 import googleLogo from "./assets/google_logo.jpg";
 import { Viewer } from "../../lib/types";
 import { AUTH_URL } from "../../lib/graphql/queries/AuthUrl";
 import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
+import { LOG_IN } from "../../lib/graphql/mutations/LogIn"
+import {
+  logIn as LogInData,
+  logInVariables
+} from "../../lib/graphql/mutations/LogIn/__generated__/logIn";;
 
 const { Content } = Layout;
 const { Text, Title } = Typography
@@ -14,10 +19,39 @@ interface Props {
 }
 
 export const Login = ({ setViewer }: Props) => {
+  const [logIn, {data: logInData, loading: logInLoading, error: logInError }] = useMutation<LogInData, logInVariables>(LOG_IN, {
+    onCompleted: data => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+      }
+    }
+  })
+
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: { code }
+        }
+      });
+    }
+  }, []);
   const client = useApolloClient();
-  console.log(AUTH_URL)
+
+  if(logInLoading) {
+    return (
+      <Content className="log-in">
+        <Spin size="large" tip="Logging you in..." />
+      </Content>
+    );
+  }
+
+
+
   const handleAuthorize = async () => {
-    console.log(AUTH_URL)
     try {
       const { data } = await client.query<AuthUrlData>({
         query: AUTH_URL
