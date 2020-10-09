456d013203bd648d49208d4d9a2e221f2da5c607
NHT <n.h.t.94.e@gmail.com>
2020-10-09 17:31:10 +0700

    update

--- client/src/sections/User/index.tsx
+++ client/src/sections/User/index.tsx
@@ -1,23 +1,64 @@
-import React from "react";
-import { RouteComponentProps } from 'react-router-dom';
+import React, { useState } from "react";
+import { match, RouteComponentProps } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
+import { Col, Layout, Row } from "antd";
 import { USER } from '../../lib/graphql/queries'
 import { User as UserData, UserVariables } from '../../lib/graphql/queries/User/__generated__/User'
+import { UserProfile } from "./components";
+import { Viewer } from "../../lib/types";
+import { ErrorBanner, PageSkeleton } from "../../lib/components";
 
 interface MatchParams {
   id: string;
 }
 
 
-export const User = ({ match }: RouteComponentProps<MatchParams>) => {
+interface Props {
+  viewer: Viewer;
+}
+
+const { Content } = Layout;
+const PAGE_LIMIT = 4;
+
+export const User = ({ viewer, match }: Props & RouteComponentProps<MatchParams>) => {
+  const [listingsPage, setListingsPage] = useState(1);
+  const [bookingsPage, setBookingsPage] = useState(1);
   const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
     variables: {
-      id: match.params.id
+      id: match.params.id,
+      bookingsPage,
+      listingsPage,
+      limit: PAGE_LIMIT
     }
   })
 
-  const user = data ? data.user : null;
-  // const userProfileElement = user ? <UserProfile user={user} /> : null 
+  if(loading) {
+    return (
+      <Content className="user">
+        <PageSkeleton />
+      </Content>
+    )
+  }
 
-  return <h2>User</h2>;
+  if(error) {
+    return (
+      <Content>
+        <ErrorBanner description="This user may not exist or we've encountered an error. Please try again soon." />
+        <PageSkeleton />
+      </Content>
+    )
+  }
+
+  const user = data ? data.user : null;
+  const viewerIsUser = viewer.id === match.params.id;
+
+  const userProfileElement = user ? <UserProfile user={user} viewerIsUser={viewerIsUser} /> : null 
+
+  return (
+    <Content className="user">
+      <Row gutter={12} type="flex" justify="space-between">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+  );
 };
